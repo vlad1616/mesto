2c70f9d7b2d5eb02c66bca707fa2be2ef235fbc6
Vlad <vladmagdesyan@yandex.ru>
2023-07-12 20:48:38 +0300

    Добавил Webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './pages/index.css';
+const jordanImage = new URL('images/header-logo.svg', import.meta.url);
+const jamesImage = new URL('images/profile-avatar.png', import.meta.url);
+const bryantImage = new URL('images/popup-image.png', import.meta.url)
+
+const whoIsTheGoat = [
+  { name: 'Michael Jordan', link: jordanImage },
+  { name: 'Lebron James', link: jamesImage },
+  { name: 'Kobe Bryant', link: bryantImage },
+]; 
+  
+
